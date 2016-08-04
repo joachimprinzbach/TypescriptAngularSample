78395a8a23fcc4f0a96815c67802b6730a24a37b
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2016-08-04 22:52:37 +0200

    Use onInit lifecycle hook for component.

--- app/animals/animals.component.ts
+++ app/animals/animals.component.ts
@@ -1,20 +1,24 @@
 import {AnimalService} from "./animal.service";
 import {Animal} from "./animal";
+import IComponentController = angular.IComponentController;
+
+class animalsComponentController implements IComponentController {
+
+    animals: Animal[];
+
+    constructor(private animalService: AnimalService) {
+    }
+
+    $onInit() {
+        this.animalService.getAnimals()
+            .then(animals => this.animals = animals);
+    }
+}
 
 const animalsComponent = {
     controller: animalsComponentController,
     controllerAs: 'vm',
     templateUrl: 'app/animals/animals.html'
-}
-
-class animalsComponentController {
-
-    animals: Animal[];
-
-    constructor(private animalService: AnimalService) {
-        this.animalService.getAnimals()
-            .then(animals => this.animals = animals);
-    }
 }
 
 export const animalsComponentModule = angular.module('animalsComponentModule', [])
